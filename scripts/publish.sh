404d6b0ccb0ef3d203bf0c0c8e0f749a19448a27
FoggDev <foggdev@gmail.com>
2023-04-03 00:40:33 -0700

    Adding versioning and workflows

---
+++ scripts/publish.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+lastCommit=`git log -1 --pretty=oneline`
+fix='fix'
+feat='feat'
+major='major'
+
+# if [[ "$lastCommit" == *"$feat"* ]]; then
+#   execute=`npm version minor`
+#   echo "Creating a feature version: $execute"
+#   push=`git push origin main -f`
+#   echo "$push"
+#   publish=`npm publish`
+#   echo "$publish"
+#   exit
+# fi
+
+# if [[ "$lastCommit" == *"$major"* ]]; then
+#   execute=`npm version major`
+#   echo "Creating a major version: $execute"
+#   push=`git push origin main -f`
+#   echo "$push"
+#   publish=`npm publish`
+#   echo "$publish"
+#   exit
+# fi
+
+# if [[ "$lastCommit" == *"$fix"* ]]; then
+#   execute=`npm version patch`
+#   echo "Creating a major version: $execute"
+#   push=`git push origin main -f`
+#   echo "$push"
+#   publish=`npm publish`
+#   echo "$publish"
+#   exit
+# fi
+
+exit
