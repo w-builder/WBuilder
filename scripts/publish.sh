f1518e907013402f3437b2394b420fffaf6a6eb0
FoggDev <foggdev@gmail.com>
2023-04-09 19:34:23 -0700

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,80 +1,41 @@
 #!/bin/bash
-lastCommit=`git log -1 --pretty=oneline`
-fix='fix'
-feat='feat'
-major='major'
 
-createProject='create-project'
-devtools='devtools'
-i18n='i18n'
-utils='utils'
+lastCommit=$(git log -1 --pretty=oneline)
+
+types=("fix" "feat" "major")
+packages=("create-project" "devtools" "i18n" "utils")
 
 echo $lastCommit
 
-# if [[ "$lastCommit" == *"$feat"* ]]; then
-#   execute=`npm version minor`
-#   echo "Creating a feature version: $execute"
-#   push=`git push origin main -f`
-#   echo "$push"
-#   publish=`npm publish`
-#   echo "$publish"
-#   exit
-# fi
+version_package() {
+  package=$1
+  version_type=$2
 
-# if [[ "$lastCommit" == *"$major"* ]]; then
-#   execute=`npm version major`
-#   echo "Creating a major version: $execute"
-#   push=`git push origin main -f`
-#   echo "$push"
-#   publish=`npm publish`
-#   echo "$publish"
-#   exit
-# fi
+  case $version_type in
+    "fix") version_command="patch" ;;
+    "feat") version_command="minor" ;;
+    "major") version_command="major" ;;
+    *) echo "Invalid version type"; exit 1 ;;
+  esac
 
-if [[ "$lastCommit" == *"$fix"* ]]; then
-  if [[ "$lastCommit" == *"$createProject"* ]]; then
-    echo "Versioning @wbuilder/create-project"
-    execute=`cd packages/create-project && npm run lint && npm version patch && git add . && git commit -m "Versioning @wbuilder/create-project"`
-    echo "Creating a patch version: $execute"
-    push=`git push origin main -f`
-    echo "$push"
-    publish=`cd packages/create-project && npm publish`
-    echo "$publish"
-    exit
+  echo "Versioning @wbuilder/$package ($version_type)"
+  execute=$(cd packages/$package && npm run lint && npm version $version_command && git add . && git commit -m "Versioning @wbuilder/$package ($version_type)")
+  echo "Creating a $version_command version: $execute"
+  push=$(git push origin main -f)
+  echo "$push"
+  publish=$(cd packages/$package && npm publish)
+  echo "$publish"
+}
+
+for type in "${types[@]}"; do
+  if [[ "$lastCommit" == *"$type"* ]]; then
+    for package in "${packages[@]}"; do
+      if [[ "$lastCommit" == *"$package"* ]]; then
+        version_package $package $type
+        exit
+      fi
+    done
   fi
+done
 
-  if [[ "$lastCommit" == *"$devtools"* ]]; then
-    echo "Versioning @wbuilder/devtools"
-    execute=`cd packages/devtools && npm run lint && npm version patch && git add . && git commit -m "Versioning @wbuilder/devtools"`
-    echo "Creating a patch version: $execute"
-    push=`git push origin main -f`
-    echo "$push"
-    publish=`cd packages/devtools && npm publish`
-    echo "$publish"
-    exit
-  fi
-
-  if [[ "$lastCommit" == *"$i18n"* ]]; then
-    echo "Versioning @wbuilder/i18n"
-    execute=`cd packages/i18n && npm run lint && npm version patch && git add . && git commit -m "Versioning @wbuilder/i18n"`
-    echo "Creating a patch version: $execute"
-    push=`git push origin main -f`
-    echo "$push"
-    publish=`cd packages/i18n && npm publish`
-    echo "$publish"
-    exit
-  fi
-
-  if [[ "$lastCommit" == *"$utils"* ]]; then
-    echo "Versioning @wbuilder/utils"
-    execute=`cd packages/utils && npm run lint && npm version patch && git add . && git commit -m "Versioning @wbuilder/utils"`
-    echo "Creating a patch version: $execute"
-    push=`git push origin main -f`
-    echo "$push"
-    publish=`cd packages/utils && npm publish`
-    echo "$publish"
-    exit
-  fi
-  exit
-fi
 exit
